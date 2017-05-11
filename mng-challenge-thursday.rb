1300888d497c07112d80787ce49c30b52e582f7c
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-11 10:46:26 -0400

    Adds Chris' solution which uses select method on the input array.

--- mng-challenge-thursday.rb
+++ mng-challenge-thursday.rb
@@ -8,4 +8,13 @@ def even_array(num_array)
 	new_array
 end
 
+
+# Chris' soln
+def even_array_select(num_array)
+	num_array = num_array.select do |num|
+		num % 2 == 0
+	end
+end
+
 p even_array([0, 0.1, 2, 3, 81, 17, 16.0])
+p even_array_select([0, 0.1, 2, 3, 81, 17, 16.0])
